2568fa49bee6ad4ddccbafb0b61c6b3d8ff14ee3
2101031000177/Tapan <2101031000177@silveroakuni.ac.in>
2023-10-06 22:45:43 +0530

    php files

---
+++ php/update_class.php
@@ -0,0 +1,5 @@
+<?php session_start(); ?>
+<?php include 'node_class.php'; ?>
+<?php
+  
+?>
--- take.php
+++ take.php
@@ -73,8 +73,8 @@
 			<li><a href="profile.php">Profile</a></li>
             
 			<li><a href="statistics.php">Statistics</a></li>
-			<li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+			<!-- <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li> -->
 			<li><a href="logout.php">Logout</a></li>
           
           </ul>
